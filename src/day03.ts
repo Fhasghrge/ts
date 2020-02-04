e845bb72e54f78a4a82c1d707411cb5347c93673
Fhasghrge <1969533391@qq.com>
2020-02-04 17:25:05 +0800

    interface

--- src/day03.ts
+++ src/day03.ts
@@ -32,3 +32,103 @@ class Cat extends Animal {
 }
 let cc = new Cat('shuang')
 cc.eat()
+
+interface person {
+  firstName: string;
+  lastName: string
+}
+
+function Teacher(man: person) {
+  console.log(man.firstName + '-----' + man.lastName)
+}
+
+// Teacher({ // 对象中只能有接口中要求的数据
+//   firstName:'zhang',
+//   lastName : 'shuang'
+// })
+
+let obj = { // 对象中必须要有接口中要求的数据
+  firstName: 'zhang',
+  lastName: 'shuang',
+  age: 122
+}
+Teacher(obj)
+
+
+// 函数类型接口
+interface jiami {
+  (key: string, value: string) : string
+}
+let md: jiami = function(key: string, value: string): string {
+  return key + ' jiami ' + value
+}
+console.log(md('zhang', 'shuang'))
+
+// 数组的约束
+interface arr {
+  [index :number] :string
+}
+let arrdemo :arr = ["a", 'b']
+console.log(arrdemo[0])
+
+// 对象的越是
+interface obb {
+  [index :string] :string
+}
+let obbdemo :obb = {
+  name: 'zhang',
+  age: '33'
+}
+console.log(obbdemo.name)
+
+// 类类型接口
+interface AnimalClass {
+  name :string
+  eat (str :string) :void
+}
+class Dog implements AnimalClass {
+  name :string
+  constructor (name :string) {
+    this.name = name;
+  }
+  eat() { // 可以不传入参数
+    console.log('dog eat')
+  }
+}
+let aaaa = new Dog('shshsshs')
+aaaa.eat()
+
+// 接口的继承
+interface BinDu {
+  eat() :void
+}
+interface xiJun extends BinDu {
+  work() :void
+}
+
+class xinXin implements xiJun {
+  public name :string
+  constructor(name :string) {
+    this.name = name
+  }
+  eat() {
+    console.log(`${this.name} is eating`)
+  }
+  work() {
+    console.log(`${this.name} is working`)
+  }
+}
+let ri = new xinXin("exing")
+ri.eat()
+ri.work()
+class sars extends xinXin implements xiJun {
+  constructor (name :string) {
+    super(name)
+  }
+  eat() {
+    console.log('extends implements class')
+  }
+  work() {
+    console.log('extends implements class')
+  }
+}
