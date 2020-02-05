54e37cad89b9b9ba1ae667ded1e12504e0993370
Fhasghrge <1969533391@qq.com>
2020-02-05 13:31:20 +0800

    装饰器

--- src/day04.ts
+++ src/day04.ts
@@ -6,3 +6,61 @@ namespace A{
   }
 }
 A.aaa()
+
+// 类装饰器
+function logClass(param: any) { // param 就是当前类
+  console.log(param)
+
+  param.prototype.name = 'fffff' // 扩展类
+}
+
+// 普通装饰器， 无法传参
+@logClass
+class httpClient {
+  constructor () {
+
+  }
+  getData () {
+
+  }
+}
+
+let httpdemo: any = new httpClient()
+console.log(httpdemo.name)
+
+// 装饰器工厂
+function loginClass(param :string) {
+  return function (target: any) {
+    console.log(target)
+    console.log(param)
+    target.run = function () {
+      console.log('i am loginClass')
+    }
+  }
+}
+@loginClass('装饰器')
+class login {
+  constructor () {
+
+  }
+  getData() {
+
+  }
+}
+let logindemo: any= new login()
+logindemo.run()
+
+// 类装饰器重载
+function login2 (param: any) {
+  return class extends login {
+    //在这里重载类
+  }
+}
+function logProperty (param: any) {
+  return function (target: any, attr: any) {
+    console.log(target)
+    console.log(attr)
+    target.attr = param
+  }
+}
+
